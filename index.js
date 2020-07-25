fe36aa82e5b20f9e64663313a99740ad01fcb50d
Saurabh Totey <SaurabhTotey@gmail.com>
2020-07-25 12:17:53 -0600

    Outputs

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ updateWyrmiteCost();
  * For explanations, see https://github.com/SaurabhTotey/Dragalia-Lost-Summon-Strategy/blob/master/Strategy.ipynb
  */
 
+// Utility functions
 const productOfRange = (start, stop) => [...Array(stop - start + 1).keys()].reduce((product, value) => product * (value + start), 1);
 const nCr = (n, r) => {
 	const biggerR = Math.max(r, n - r);
@@ -40,6 +41,7 @@ const nCr = (n, r) => {
 const pow = (a, b) => [...Array(b)].reduce(product => product * a, 1);
 const tenChooseTable = [...Array(11).keys()].map(i => nCr(10, i));
 
+// Expected value function
 function E(n, r, s, l, p, q, lookupTable = {}) {
 	const key = `${n},${r},${l}`;
 	if (key in lookupTable) {
@@ -75,18 +77,28 @@ function E(n, r, s, l, p, q, lookupTable = {}) {
 /**
  * Handles managing the parameters and calculations and updating the page with the relevant information
  */
+
 const summaryParagraph = document.getElementById("summary");
 const pityIncreaseInput = document.getElementById("pityIncrease");
 
 async function refresh() {
 	summaryParagraph.innerText = "Loading...";
 
+	// Pull relevant parameters from form
 	const numberSummons = parseInt(numberSummonsInput.value);
 	const baseRate = Number(summonRateTypeSelector.options[summonRateTypeSelector.selectedIndex].value);
 	const pityIncrease = Number(pityIncreaseInput.value);
 
+	// Perform calculations asynchronously
 	const calculations = [...Array(Math.floor(numberSummons / 10) + 1).keys()].map(async strategy => E(numberSummons, baseRate, strategy, 0, baseRate, pityIncrease));
 	const expectedValues = await Promise.all(calculations);
-	console.log(expectedValues);
+	const bestStrategy = expectedValues.reduce((indexOfMax, value, i) => value > expectedValues[indexOfMax] ? i : indexOfMax, 0);
+
+	// TODO: scatter plot for data
+
+	// Updates the output summary
+	summaryParagraph.innerText = `The best strategy for ${numberSummons} is to perform ${bestStrategy * 10} single summons before only doing tenfold summons.`
+		+ ` Once a 5-star unit has been obtained, restart from the beginning of the strategy, even if the 5-star unit was obtained with single summons.`
+		+ ` With this strategy, you can expect around ${expectedValues[bestStrategy]} 5-star units on average.`;
 }
 refresh();
